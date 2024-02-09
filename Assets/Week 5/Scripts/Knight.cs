0e65c2b08ea52ca8a846e01beadb023ffffec093
dolrah <148375495+dolrah@users.noreply.github.com>
2024-02-09 17:55:32 -0500

    health

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -9,17 +9,25 @@ public class Knight : MonoBehaviour
     public float speed = 3f;
     Rigidbody2D rb;
 
+    public float health;
+    public float maxHealth = 5f;
+
+    bool isDead = false;    
+
+    bool clickingOnSelf = false;
+
     Animator animator;
 
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();  
         animator = GetComponent<Animator>();
+        health = maxHealth;
     }
 
     private void FixedUpdate()
     {
+        //deals with the movement
         movement = destination - (Vector2)transform.position;
 
         if(movement.magnitude < 0.1)
@@ -30,12 +38,41 @@ public class Knight : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) )
+        if (Input.GetMouseButtonDown(0) && !clickingOnSelf )
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         }
         animator.SetFloat("Movment", movement.magnitude);
 
 
+    }
+
+    public void Takedamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        if (health == 0)
+        {
+            isDead = true;
+            animator.SetTrigger("death");
+        }
+        else
+        {
+            isDead =false;
+            animator.SetTrigger("TakeDamage");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isDead) return;
+        clickingOnSelf = true;
+        // makes it so the entire system know you took damage
+        //SendMessage("TakeDamage", 1);
+    }
+    private void OnMouseUp()
+    {
+        clickingOnSelf = false;
     }
 }
