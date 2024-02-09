6c869c1ceb03150e474e1c0362138fdd1e141b1e
dolrah <148375495+dolrah@users.noreply.github.com>
2024-02-09 17:12:58 -0500

    Getting help!!

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    Vector2 movement;
+    Vector2 destination;
+    public float speed = 3f;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
