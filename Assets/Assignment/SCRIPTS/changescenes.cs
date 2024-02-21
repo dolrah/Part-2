7a6a87a4d23999b320b5461d0be2d8c104e265f5
dolrah <148375495+dolrah@users.noreply.github.com>
2024-02-21 17:17:07 -0500

    Added the startng stuff for assigment 1

---
+++ Assets/Assignment/SCRIPTS/changescenes.cs
@@ -0,0 +1,12 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//lets you do stuff with the scenes
+using UnityEngine.SceneManagement;
+
+public class changescenes : MonoBehaviour
+{
+    
+}
