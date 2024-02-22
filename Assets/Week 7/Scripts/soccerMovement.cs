19dca4144d6f58bef445b2752fae93999bb29932
dolrah <148375495+dolrah@users.noreply.github.com>
2024-02-22 10:14:17 -0500

    highlights the player when it is clicked on

--- Assets/Week 7/Scripts/soccerMovement.cs
+++ Assets/Week 7/Scripts/soccerMovement.cs
@@ -6,15 +6,39 @@ using UnityEngine;
 
 public class soccerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    SpriteRenderer playerselected;
+    bool clickingOnSelf;
+
+    private void Start()
     {
-        
+        playerselected = GetComponent<SpriteRenderer>();
+        playerselected.color = Color.white;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        seleceted(clickingOnSelf);
+    }
+
+    public void seleceted(bool clickingOnSelf)
+    {
+        if (clickingOnSelf == true)
+        {
+            print("clicking on self");
+            playerselected.color = Color.cyan;
+        }
+        else { playerselected.color = Color.white; }
+    }
+
+    private void OnMouseDown()
+    {
+        print("mousedown");
+        clickingOnSelf = true;
+    }
+
+    private void OnMouseUp()
+    {
+        print("mouseup");
+        clickingOnSelf = false;
     }
 }
